566b2a15539e4e0e76810379e37030b7b5bfd670
Henri Karhu <karhu.henri@gmail.com>
2014-05-12 21:03:30 +0300

    tehtava aloitettu

---
+++ javakurssi/src/vk14/YstavaSovellus.java
@@ -0,0 +1,77 @@
+package vk14;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class YstavaSovellus {
+
+	public static void main(String[] args) {
+
+		HashMap<String, Ystava> ystavat = new HashMap<>();
+
+		Scanner lukija = new Scanner(System.in);
+
+		int toiminto;
+
+		boolean olenkoPaalla = true;
+
+		while (true) {
+			System.out
+					.print("1) Lisää ystävä\n2) Etsi ystävä\n3) Poista Ystävä\n4) Tulosta ystävät\n0) Lopeta");
+			toiminto = lukija.nextInt();
+
+			switch (toiminto) {
+			case 1:
+				lisaaYstava();
+				break;
+			case 2:
+				etsiYstava();
+				break;
+			case 3:
+				poistaYstava();
+				break;
+			case 4:
+				tulostaYstava();
+				break;
+			case 0:
+				olenkoPaalla = false;
+				break;
+			default:
+				break;
+			}
+		}
+
+	}
+
+	private static void tulostaYstava() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void poistaYstava() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void etsiYstava() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void lisaaYstava() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+class Ystava {
+	String lempinimi;
+	String nimi;
+	String puhelin;
+	String syntymapaivamaara;
+
+	public Ystava() {
+
+	}
+}
